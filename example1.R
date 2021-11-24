e0890962ce66be64ff2d11db65f0eb5bb4d073da
notblank <zert.federico@gmail.com>
2021-11-24 16:48:42 -0500

    first example

--- example1.R
+++ example1.R
@@ -7,7 +7,7 @@ O_dist <- function(n) {
 }
 
 ## Observed data:
-n_obs <- 10
+n_obs <- 100
 Obs <- O_dist(n_obs)
 
 
@@ -30,14 +30,16 @@ Obs_freq <- tibble(Obs) %>%
 Obs_hat <- rep(0, 4)
 Obs_hat[Obs_pos] <- Obs_freq
 
-unif_lik <- 1 - 0.5 * sum(abs(Obs_hat - rep(1/4, 4)))
+unif_lik <- 1 - sum(abs(Obs_hat - rep(1/4, 4)))
 
-base_rates <- c(2/4, 1/4, 0, 1/4)
-base_rates_lik <- 1 - 0.5 * sum(abs(Obs_hat - base_rates))
+base_rates <- c(1/4, 0, 2/4, 1/4)
+base_rates_lik <- 1 - sum(abs(Obs_hat - base_rates))
 
 post_unif <- unif_lik * unif_prior
 post_base_rates <- base_rates_lik * base_rates_prior
 
+unif_prior * log(post_unif/unif_prior, base=2)
+base_rates_prior * log(post_base_rates/base_rates_prior, base=2)
 
-log(post_unif/unif_prior, base=2)
-log(post_base_rates/base_rates_prior, base=2)
+
+
